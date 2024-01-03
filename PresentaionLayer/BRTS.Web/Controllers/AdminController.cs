3b2664f8d9350e1133a888f923fceeddabd2cd1f
abualrub03 <89502136+abualrub03@users.noreply.github.com>
2024-01-03 12:24:51 +0300

    wdq

--- PresentaionLayer/BRTS.Web/Controllers/AdminController.cs
+++ PresentaionLayer/BRTS.Web/Controllers/AdminController.cs
@@ -7,6 +7,20 @@ namespace BRTS.Web.Controllers
 
         public IActionResult AdminDashBoard()
         {
+            ViewModels.AdminViewModels.DashBoardViewModel dashBoardViewModel = new ViewModels.AdminViewModels.DashBoardViewModel(); 
+            
+            BRTS_Providers.AccountProvider.Account _account = new BRTS_Providers.AccountProvider.Account();
+            BRTS_Providers.BusesProvider.Buses _bus = new BRTS_Providers.BusesProvider.Buses();
+            BRTS_Providers.TripsProvider.Trips _trip = new BRTS_Providers.TripsProvider.Trips();
+
+            return View();
+        } 
+        public IActionResult CreateNewTrip()
+        {
+            return View();
+        }
+        public IActionResult CreateNewTripRequest()
+        {
             return View();
         }
 
